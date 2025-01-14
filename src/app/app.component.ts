0f5397b18c7a28b1c49664d88cda39a100e3edca
MB-PC\ich <martinbock@arcor.de>
2025-01-14 17:33:38 +0100

    "header, footer, main-content added"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
