950245a928d648d1355773f25269031757d7348b
MB-PC\ich <martinbock@arcor.de>
2025-01-15 12:58:36 +0100

    "aot section profile-img implemented"

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [LandingpageComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
