b3de5b966552e3c5061eaa2e0d6f169cc5fa6d49
MB-PC\ich <martinbock@arcor.de>
2025-01-17 17:36:02 +0100

    "image section, navigation created, responsivenes for landing-page started"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/component/header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
-import { FooterComponent } from './shared/component/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, MainContentComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/main-content/landingpage/landingpage.component.ts
+++ src/app/main-content/landingpage/landingpage.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './landingpage.component.html',
-  styleUrl: './landingpage.component.scss'
+  styleUrl: './landingpage.component.scss',
 })
 export class LandingpageComponent {
+  activeLang: string = 'DE'; // Standardmäßig DE aktiv
+  activeSection: string = ''; // Standardmäßig keins aktiv
+  respMenuClosed: boolean = true;
 
+  setActive(lang: string): void {
+    this.activeLang = lang;
+  }
+
+  setActiveSection(section: string): void {
+    this.activeSection = section;
+  }
+
+  toggleRespMenu(): void {
+    this.respMenuClosed = !this.respMenuClosed;
+  }
 }
