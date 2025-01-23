2cfd73401161871ff280cacd2260960ff3b6b2f5
MB-PC\ich <martinbock@arcor.de>
2025-01-23 16:05:41 +0100

    "my skills mobile design created"

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LandingpageComponent } from './landingpage/landingpage.component';
-import { HeaderComponent } from '../shared/comonent/header/header.component';
+import { HeaderComponent } from '../shared/component/header/header.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 
