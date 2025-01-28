266b284e6bd68587d8688a3a2e391290d9bd86e1
MB-PC\ich <martinbock@arcor.de>
2025-01-28 18:14:06 +0100

    "contact section started"

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,17 +1,31 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
   constructor(private translate: TranslateService) {}
-    changeLanguage(language: string) {
-      this.translate.use(language);
+  changeLanguage(language: string) {
+    this.translate.use(language);
+  }
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
+
+  onSubmit(NgForm: NgForm) {
+    if(NgForm.valid && NgForm.submitted) {
+      console.log(this.contactData);
     }
   }
+}
   
