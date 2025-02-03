63e4d4e1970061ba0902a7f3c5f5f1bf731e6d43
MB-PC\ich <martinbock@arcor.de>
2025-02-03 18:21:53 +0100

    "some fixes"

--- src/app/main-content/why-me/why-me.component.ts
+++ src/app/main-content/why-me/why-me.component.ts
@@ -1,53 +1,3 @@
-// import { Component } from '@angular/core';
-// import {
-//   TranslateDirective,
-//   TranslatePipe,
-//   TranslateService,
-// } from '@ngx-translate/core';
-
-// @Component({
-//   selector: 'app-why-me',
-//   standalone: true,
-//   imports: [TranslatePipe],
-
-//   templateUrl: './why-me.component.html',
-//   styleUrl: './why-me.component.scss',
-// })
-// export class WhyMeComponent {
-//   constructor(private translate: TranslateService) {}
-//   changeLanguage(language: string) {
-//     this.translate.use(language);
-//   }
-// }
-
-// import { CommonModule } from '@angular/common';
-// import { Component, OnInit } from '@angular/core';
-// import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-
-// @Component({
-//   selector: 'app-why-me',
-//   standalone: true,
-//   imports: [CommonModule, TranslatePipe],
-//   templateUrl: './why-me.component.html',
-//   styleUrls: ['./why-me.component.scss'],
-// })
-// export class WhyMeComponent implements OnInit {
-//   showLocateIcon = true;
-
-//   constructor(private translate: TranslateService) {}
-
-//   ngOnInit() {
-//     // Setze ein Intervall, um die Variable regelmäßig zu ändern (alle 3 Sekunden)
-//     setInterval(() => {
-//       this.showLocateIcon = !this.showLocateIcon;
-//     }, 3000); // Wechsel alle 3 Sekunden
-//   }
-
-//   changeLanguage(language: string) {
-//     this.translate.use(language);
-//   }
-// }
-
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
@@ -67,7 +17,7 @@ export class WhyMeComponent implements OnInit {
   ngOnInit() {
     setInterval(() => {
       this.showLocateIcon = !this.showLocateIcon;
-    }, 8000);
+    }, 12000);
   }
 
   changeLanguage(language: string) {
