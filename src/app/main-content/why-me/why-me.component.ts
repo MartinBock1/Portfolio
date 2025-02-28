0b36e458426cbec206f430ae8d563a5e217548a0
Martin Bock <martinbock@arcor.de>
2025-02-28 15:42:26 +0100

    "why me relocate option added"

--- src/app/main-content/why-me/why-me.component.ts
+++ src/app/main-content/why-me/why-me.component.ts
@@ -10,13 +10,15 @@ import { TranslatePipe, TranslateService } from '@ngx-translate/core';
   styleUrls: ['./why-me.component.scss'],
 })
 export class WhyMeComponent implements OnInit {
-  showLocateIcon = true;
+  // showLocateIcon = true;
+  currentState = 0; // 0: Locate, 1: Remote, 2: Suitcase
 
   constructor(private translate: TranslateService) {}
 
   ngOnInit() {
     setInterval(() => {
-      this.showLocateIcon = !this.showLocateIcon;
+      // this.showLocateIcon = !this.showLocateIcon;
+      this.currentState = (this.currentState + 1) % 3; // We cycle through 3 states (0, 1, 2)
     }, 12000);
   }
 
@@ -24,7 +26,32 @@ export class WhyMeComponent implements OnInit {
     this.translate.use(language);
   }
 
+  // get typewriterText() {
+  //   return this.showLocateIcon ? 'why_me.location' : 'why_me.remote';
+  // }
   get typewriterText() {
-    return this.showLocateIcon ? 'why_me.location' : 'why_me.remote';
+    switch (this.currentState) {
+      case 0:
+        return 'why_me.location';
+      case 1:
+        return 'why_me.remote';
+      case 2:
+        return 'why_me.suitcase';
+      default:
+        return '';
+    }
+  }
+
+  get iconSrc() {
+    switch (this.currentState) {
+      case 0:
+        return 'assets/img/icons/locate_icon.svg';
+      case 1:
+        return 'assets/img/icons/remote_icon.svg';
+      case 2:
+        return 'assets/img/icons/suitcase_icon.svg';
+      default:
+        return '';
+    }
   }
 }
